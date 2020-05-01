a6f02f19394922a971fb7a1b91b39cc08d953e81
Peter <sheraium@gmail.com>
2020-05-01 11:59:05 +0800

    in memory running

---
+++ src/IdentityServer/Configuration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityModel;
+using IdentityServer4.Models;
+
+namespace IdentityServer
+{
+    public class Configuration
+    {
+        public static IEnumerable<ApiResource> GetApis() =>
+            new List<ApiResource>()
+            {
+                new ApiResource("ApiOne"),
+            };
+
+        public static IEnumerable<Client> GetClients =>
+            new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId = "client_id",
+                    ClientSecrets = {new Secret("client_secret".ToSha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"ApiOne"},
+                }
+            };
+    }
+}
--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -16,6 +16,12 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
+            services.AddIdentityServer()
+                .AddInMemoryApiResources(Configuration.GetApis())
+                .AddInMemoryClients(Configuration.GetClients)
+                .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +32,13 @@ namespace IdentityServer
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseIdentityServer();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
